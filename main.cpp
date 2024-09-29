a855bd035df08d447d5406647eaa244520bfcd12
alee3x <aleeex3@icloud.com>
2024-09-30 00:56:39 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 #include "my_add.hpp"
 
 /**
-* \brief This function accepts two arguements and returns their sum.
-* \return Sum of two integers.
-*/
+ * @brief This function accepts two arguements and returns their sum.
+ * @return Sum of two integers.
+ */
 int main(int argc, char *argv[]) {
   if (argc != 3) {
-    //  Check for exactly 2 arguements entered.
+    // @brief Check for exactly 2 arguements entered.
     std::cerr << "Provide exactly 2 arguments\n";
     return 1;
   }
-  // Accept 2 arguements from standard input
+  // @brief Accept 2 arguements from standard input
   int a = std::atoi(argv[1]);
   int b = std::atoi(argv[2]);
   std::cout << my_add(a, b) << '\n';
