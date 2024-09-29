3390c17b40bed27719b8ee78f357d316b34fd88a
alee3x <aleeex3@icloud.com>
2024-09-29 23:29:02 +0300

    update

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 int main(int argc, char *argv[]) {
   if (argc != 3) {
     std::cerr << "Provide exactly 2 arguments\n";
-    return -1;
+    return 1;
   }
   int a = std::atoi(argv[1]);
   int b = std::atoi(argv[2]);
-  std::cout << my_add(a, b) << "\n";
+  std::cout << my_add(a, b) << '\n';
   return 0;
 }
