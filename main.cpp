dd19ae25592d4ad2285e9d6646b78b52c9d7fc03
alee3x <aleeex3@icloud.com>
2024-09-29 23:47:40 +0300

    trying Doxygen

--- main.cpp
+++ main.cpp
@@ -3,11 +3,17 @@
 
 #include "my_add.hpp"
 
+/**
+* \brief This function accepts two arguements and returns their sum.
+* \return Sum of two integers.
+*/
 int main(int argc, char *argv[]) {
   if (argc != 3) {
+    //  Check for exactly 2 arguements entered.
     std::cerr << "Provide exactly 2 arguments\n";
     return 1;
   }
+  // Accept 2 arguements from standard input
   int a = std::atoi(argv[1]);
   int b = std::atoi(argv[2]);
   std::cout << my_add(a, b) << '\n';
