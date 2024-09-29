8fff909c0226b8f9d3f249c7eca8f65848f535dc
alee3x <aleeex3@icloud.com>
2024-09-30 01:30:03 +0300

    Another

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 #include <cstdlib>
 #include <iostream>
 
+/** @file
+ * @brief Brief explanation of this file
+ *
+ * This is a detailed explaination of this file
+ */
+
 #include "my_add.hpp"
 
-/**
- * @brief This function accepts two arguements and returns their sum.
- * @return Sum of two integers.
- */
 int main(int argc, char *argv[]) {
+  /**
+   * @brief Check for exactly 2 arguements entered.
+   */
   if (argc != 3) {
-    // @brief Check for exactly 2 arguements entered.
     std::cerr << "Provide exactly 2 arguments\n";
     return 1;
   }
-  // @brief Accept 2 arguements from standard input
+  /**
+   * @brief Convert 2 arguements from standard input
+   */
   int a = std::atoi(argv[1]);
   int b = std::atoi(argv[2]);
   std::cout << my_add(a, b) << '\n';
--- src/my_add.cpp
+++ src/my_add.cpp
@@ -1,1 +1,10 @@
+/**
+ * @brief Adds 2 numbers
+ *
+ * Brief explaination of how it adds 2 numbers
+ *
+ * @param a variable 1
+ * @param b variable 2
+ * @return sum of a and b
+ */
 int my_add(int a, int b) { return a + b; }
