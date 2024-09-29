93e7fcdd5387797797be07e3d94d661d70111443
alee3x <aleeex3@icloud.com>
2024-09-30 01:47:46 +0300

    FINAL DOXYGEN

--- main.cpp
+++ main.cpp
@@ -9,17 +9,18 @@
 
 #include "my_add.hpp"
 
+/**
+ * @brief Main function
+ *
+ * Checks for EXACTLY two arguements entered,
+ * as
+ */
 int main(int argc, char *argv[]) {
-  /**
-   * @brief Check for exactly 2 arguements entered.
-   */
   if (argc != 3) {
     std::cerr << "Provide exactly 2 arguments\n";
     return 1;
   }
-  /**
-   * @brief Convert 2 arguements from standard input
-   */
+
   int a = std::atoi(argv[1]);
   int b = std::atoi(argv[2]);
   std::cout << my_add(a, b) << '\n';
